f628ac845c69a809af838b08d8e974f1a8466067
Julienm25 <julienmauvais@hotmail.fr>
2020-06-19 12:36:02 +0200

    Page welcome + bootstrap

--- src/app/accueil1/accueil1.component.ts
+++ src/app/accueil1/accueil1.component.ts
@@ -3,7 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-accueil1',
   templateUrl: './accueil1.component.html',
-  styleUrls: ['./accueil1.component.css']
+  styleUrls: ['./accueil1.component.css'],
+  styles: [`
+    body{
+      height: 100%;
+      position: absolute;
+      width: 50%;
+      left: 0;
+    }
+
+`]
+
 })
 export class Accueil1Component implements OnInit {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBar1Component } from './nav-bar1/nav-bar1.component';
 import { Accueil1Component } from './accueil1/accueil1.component';
-import { Accueil2Component } from './accueil2/accueil2.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBar1Component,
     Accueil1Component,
-    Accueil2Component
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
