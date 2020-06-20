8a14aa3f22114f57318b609c9ce8c94c1c19c8d4
Julienm25 <julienmauvais@hotmail.fr>
2020-06-20 12:20:27 +0200

    Page Main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InscriptionComponent} from "./inscription/inscription.component";
+import { AccueilComponent } from './accueil/accueil.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NosActivitesComponent } from './nos-activites/nos-activites.component';
+import { MaCommunauteComponent } from './ma-communaute/ma-communaute.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  {path: 'inscription', component: InscriptionComponent},
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'nos-activites', component: NosActivitesComponent},
+  {path: 'ma-communaute', component: MaCommunauteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Accueil1Component } from './accueil1/accueil1.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NosActivitesComponent } from './nos-activites/nos-activites.component';
+import { MaCommunauteComponent } from './ma-communaute/ma-communaute.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Accueil1Component,
+    AccueilComponent,
+    InscriptionComponent,
     WelcomeComponent,
+    NavbarComponent,
+    NosActivitesComponent,
+    MaCommunauteComponent,
   ],
   imports: [
     BrowserModule,
